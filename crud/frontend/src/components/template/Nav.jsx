25c3f043e46b4a4d708be224ad9ddcff45572d91
Josimar Ferreira <josimarferreira922@yahoo.com.br>
2022-01-30 12:39:10 -0300

    Refatorando componente Nav

--- crud/frontend/src/components/template/Nav.jsx
+++ crud/frontend/src/components/template/Nav.jsx
@@ -1,7 +1,9 @@
 import './Nav.css'
 import React from 'react'
+import NavItem  from './NavItem'
 
 export default props => 
     <aside className="menu-area">
-        Navegação
+        <NavItem  link="#/" icon="home" label="Início"/>
+        <NavItem link="#/users" icon="users" label="Usuários" />
     </aside>
