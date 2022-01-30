6df9989ca7ede98d04a850cf6a5001a5c9ea2a3e
Josimar Ferreira <josimarferreira922@yahoo.com.br>
2022-01-30 13:08:20 -0300

    Criando o componente Home

--- crud/frontend/src/main/App.jsx
+++ crud/frontend/src/main/App.jsx
@@ -1,17 +1,18 @@
-import './App.css'
-import React from 'react'
-import 'font-awesome/css/font-awesome.min.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import "./App.css";
+import React from "react";
+import "font-awesome/css/font-awesome.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import Logo from '../components/template/Logo'
-import Nav from '../components/template/Nav'
-import Main from '../components/template/Main'
-import Footer from '../components/template/Footer'
+import Logo from "../components/template/Logo";
+import Nav from "../components/template/Nav";
+import Home from '../components/home/Home'
+import Footer from "../components/template/Footer";
 
-export default props =>
-    <div className='app'>
-        <Logo />
-        <Nav />
-        <Main icon="home" title="Início" subtitle="Segundo projeto do capítulo de React"/>
-        <Footer />
-    </div>
+export default (props) => (
+  <div className="app">
+    <Logo />
+    <Nav />
+    <Home/>
+    <Footer />
+  </div>
+);
